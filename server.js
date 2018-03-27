8214ff3ae17cd09a89e231d6c04f39bde69dc5f5
Ronnie Olesco <ronnie.olesco@gmail.com>
2018-03-28 02:37:25 +0800

    Initial commit

---
+++ server.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+//middleware
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+
+// custom middle ware
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if (err){
+            console.log ('Unable to append to server.log');
+        }
+    });
+    next();
+});
+//MAINTENANCE MIDDLEWARE
+//app.use((req, res, next) => {
+//    res.render('maintenance.hbs');
+//});
+app.use(express.static(__dirname + '/public'));
+
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();s
+});
+
+//
+app.get('/', (req, res) =>{
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        welcomeMessage: 'Welcome to my Website'
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About Page',
+        currentYear : new Date().getFullYear()
+    });
+});
+
+app.get('/bad', (req, res) => {
+    res.send ({
+        errorMessage : 'UNABLE TO FULLFILL THIS REQUEST'
+    });
+
+});
+
+app.listen(3001, () => {
+    console.log('Server is up on port 3001');
+
+});
+
